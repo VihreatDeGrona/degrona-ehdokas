c895ebec73463e4358de3a251a73202df10831f3
Janne Saarela <janne.juhani@gmail.com>
2014-10-12 22:49:50 +0300

    Comment out colorpicker stuff

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,22 +3,22 @@
 
     /***** Colour picker *****/
 
-    $('.colorpicker').hide();
-    $('.colorpicker').each(function() {
-      $(this).farbtastic($(this).closest('.color-picker').find('.color'));
-    });
+    // $('.colorpicker').hide();
+    // $('.colorpicker').each(function() {
+    //   $(this).farbtastic($(this).closest('.color-picker').find('.color'));
+    // });
 
-    $('.color').click(function() {
-      $(this).closest('.color-picker').find('.colorpicker').fadeIn();
-    });
+    // $('.color').click(function() {
+    //   $(this).closest('.color-picker').find('.colorpicker').fadeIn();
+    // });
 
-    $(document).mousedown(function() {
-      $('.colorpicker').each(function() {
-        var display = $(this).css('display');
-        if (display == 'block')
-          $(this).fadeOut();
-      });
-    });
+    // $(document).mousedown(function() {
+    //   $('.colorpicker').each(function() {
+    //     var display = $(this).css('display');
+    //     if (display == 'block')
+    //       $(this).fadeOut();
+    //   });
+    // });
 
 
     /***** Uploading images *****/
